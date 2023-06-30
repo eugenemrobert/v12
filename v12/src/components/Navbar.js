6f79a5c402b28018130ffd0a487aed62cda45bb9
VictorBK <victorbrian4@gmail.com>
2023-06-30 10:32:57 +0300

    Responsive Navbar half done

--- v12/src/components/Navbar.js
+++ v12/src/components/Navbar.js
@@ -5,10 +5,18 @@ import { Link } from 'react-router-dom';
 import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component{
-    render(){
+    state = { clicked: false };
+    handleClick = () =>{
+        this.setState({ clicked: !this.state.clicked})
+    }
+    render() {
         return(
             <nav className='NavbarItems'>
                 <h1 className='navbar-logo'>V12</h1>
+               
+                <div className='menu-icons' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>                   
+                </div>
 
                 <ul className='nav-menu'>
                     {MenuItems.map((item, index) =>{
