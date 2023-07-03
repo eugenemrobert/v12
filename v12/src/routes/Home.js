ed9c692e07f04d4b65beb7b125e70805c51538a6
VictorBK <victorbrian4@gmail.com>
2023-07-03 11:59:53 +0300

    Home route

---
+++ v12/src/routes/Home.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function Home () {
+    return (
+        <>
+        <Navbar />
+        <h3>This is home</h3>        
+        </>
+    )
+}
+
+export default Home;
