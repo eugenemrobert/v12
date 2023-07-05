7ab170ad1da41c1d493293f47d416256d21d1d89
VictorBK <victorbrian4@gmail.com>
2023-07-05 11:22:31 +0300

    Home crossreference

--- v12/src/routes/Home.js
+++ v12/src/routes/Home.js
@@ -5,9 +5,18 @@ function Home () {
     return (
         <>
         <Navbar />
-        <Hero/>       
+        <Hero
+        cName="hero"
+        heroImg="https://images.unsplash.com/photo-1482062364825-616fd23b8fc1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+        title="Your Internet Journey Begins Here" 
+        text="Step Into a Wonderful World of the WWW"
+        buttonText="Development Plan"
+        url="/"
+        btnClass="show"
+        />
+              
         </>
-    )
+    );
 }
 
 export default Home;
