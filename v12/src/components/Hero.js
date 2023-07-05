f3679384447ad3e8c12570750e9f3ebe6ca56ae7
VictorBK <victorbrian4@gmail.com>
2023-07-05 11:21:38 +0300

    Hero modification for crossreference

--- v12/src/components/Hero.js
+++ v12/src/components/Hero.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./HeroStyles.css";
 
-function Hero() {
+function Hero(props) {
   return (<>
-    <div className='hero'>
-        <img alt='HerpImg' src='https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'/>
+    <div className={props.cName}>
+        <img alt='HerpImg' src={props.heroImg}/>
 
         <div className='hero-text'>
-            <h1>Your Internet Journey Begins Here</h1>
-            <p>Choose Your Ideal Design Today And Let's Get Started.</p>
-            <a href='/'>Development Plan</a>
+            <h1>{props.title}</h1>
+            <p>{props.text}</p>
+            <a href={props.url} className={props.btnClass}>{props.buttonText}</a>
         </div>      
     </div>
   </>);
