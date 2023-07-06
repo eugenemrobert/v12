3fc50ce69e2e86acaff0353e258d376cdaab4ca9
VictorBK <victorbrian4@gmail.com>
2023-07-06 11:20:21 +0300

    About us section dynamic hero modified

--- v12/src/routes/About.js
+++ v12/src/routes/About.js
@@ -1,6 +1,17 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
 function About () {
     return (
         <>
+        <Navbar />
+        <Hero
+        cName="hero-mid"
+        heroImg="https://cpsns.ns.ca/wp-content/uploads/2021/12/AdobeStock_338472274_sm.jpg"
+        title="Get to Know About Us" 
+        text="Find out why we do what we do the way we do it"
+        btnClass="hide"
+        />
         
         </>
     )
