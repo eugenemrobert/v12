76c5e559350e536916a1b94ef449f4fecf724e75
VictorBK <victorbrian4@gmail.com>
2023-07-06 11:20:40 +0300

    Contact section dynamic hero modified

--- v12/src/routes/Contact.js
+++ v12/src/routes/Contact.js
@@ -1,6 +1,17 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
 function Contact () {
     return (
         <>
+        <Navbar />
+        <Hero
+        cName="hero-mid"
+        heroImg="https://cpsns.ns.ca/wp-content/uploads/2021/12/AdobeStock_338472274_sm.jpg"
+        title="Get in touch with us" 
+        text="Find out why we do what we do the way we do it"
+        btnClass="hide"
+        />
         
         </>
     )
