070254e1349aa879596a7907c35b7aa5c8b26c17
VictorBK <victorbrian4@gmail.com>
2023-07-08 12:12:29 +0300

    Minor contact update

--- v12/src/routes/Contact.js
+++ v12/src/routes/Contact.js
@@ -7,7 +7,7 @@ function Contact () {
         <Navbar />
         <Hero
         cName="hero-mid"
-        heroImg="https://www.capitalfm.co.ke/thesauce/files/2022/04/Woman-on-phone.jpg"
+        heroImg="https://www.capitalfm.co.ke/thesauce/files/2022/04/Woman-on-phone.jpgsss"
         title="Get in touch with us" 
         text="Find out why we do what we do the way we do it"
         btnClass="hide"
