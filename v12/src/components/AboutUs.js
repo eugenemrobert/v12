066dd2e67087e377b5657db8bb65718f5704130e
VictorBK <victorbrian4@gmail.com>
2023-07-13 07:41:23 +0300

    About us section filler (initial)

---
+++ v12/src/components/AboutUs.js
@@ -0,0 +1,14 @@
+import "./AboutUsStyles.css";
+
+
+function AboutUs(){
+    return(
+        <div className="about-container">
+            <h1>Our History</h1>
+            <p></p>
+        </div>
+
+    )
+}
+
+export default AboutUs;
