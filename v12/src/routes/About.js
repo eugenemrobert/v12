7a72757ff70cd0fa34ab337d40f48728c7acdc71
VictorBK <victorbrian4@gmail.com>
2023-07-13 14:09:10 +0300

    About section routing

--- v12/src/routes/About.js
+++ v12/src/routes/About.js
@@ -1,3 +1,4 @@
+import AboutUs from "../components/AboutUs";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
@@ -13,6 +14,7 @@ function About () {
         text="Find out why we do what we do the way we do it"
         btnClass="hide"
         />
+        <AboutUs/>
         <Footer/>
         
         </>
