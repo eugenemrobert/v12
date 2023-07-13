a8f730529e247b01d2f0df479bc3d23d1fa77a8e
VictorBK <victorbrian4@gmail.com>
2023-07-13 14:09:24 +0300

    Contact section routing

--- v12/src/routes/Contact.js
+++ v12/src/routes/Contact.js
@@ -1,3 +1,4 @@
+import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
@@ -13,6 +14,7 @@ function Contact () {
         text="Find out why we do what we do the way we do it"
         btnClass="hide"
         />
+        <ContactForm/>
         <Footer/>
         
         </>
