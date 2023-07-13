1ffa45d09cc0f6446987cb6f27b19b972b05c9bf
VictorBK <victorbrian4@gmail.com>
2023-07-13 14:10:03 +0300

    About us initial info

--- v12/src/components/AboutUs.js
+++ v12/src/components/AboutUs.js
@@ -5,7 +5,31 @@ function AboutUs(){
     return(
         <div className="about-container">
             <h1>Our History</h1>
-            <p></p>
+            <p>V12 Technologies is a tech company that deals in web development, mobile 
+                app development, and other forms of tech support. V12 is counted
+                for its expertise in web solutions and top ranking business portals.
+                V12's invincible expertise and rich experience has raised its spirit
+                to reach ahead from its clients' expectations. Commendable success
+                rate of its other portals is a live paradigm of its work excellence.
+            </p>
+
+            <h1>Our Mission</h1>
+            <p>V12 Technologies is a tech company that deals in web development, mobile 
+                app development, and other forms of tech support. V12 is counted
+                for its expertise in web solutions and top ranking business portals.
+                V12's invincible expertise and rich experience has raised its spirit
+                to reach ahead from its clients' expectations. Commendable success
+                rate of its other portals is a live paradigm of its work excellence.
+            </p>
+
+            <h1>Our Vision</h1>
+            <p>V12 Technologies is a tech company that deals in web development, mobile 
+                app development, and other forms of tech support. V12 is counted
+                for its expertise in web solutions and top ranking business portals.
+                V12's invincible expertise and rich experience has raised its spirit
+                to reach ahead from its clients' expectations. Commendable success
+                rate of its other portals is a live paradigm of its work excellence.
+            </p>
         </div>
 
     )
