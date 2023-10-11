dc4e78cdba24b795365d56fb3935309ea53bd719
VictorBK <victorbrian4@gmail.com>
2023-10-11 19:23:43 +0300

    Edited history, vision, mission, and core values

--- v12/src/components/AboutUs.js
+++ v12/src/components/AboutUs.js
@@ -6,30 +6,65 @@ function AboutUs(){
         <div className="about-container">
             <h1>Our History</h1>
             <p>V12 Technologies is a tech company that deals in web development, mobile 
-                app development, and other forms of tech support. V12 is counted
-                for its expertise in web solutions and top ranking business portals.
-                V12's invincible expertise and rich experience has raised its spirit
-                to reach ahead from its clients' expectations. Commendable success
-                rate of its other portals is a live paradigm of its work excellence.
-            </p>
+                app development, and other forms of tech support. Started right after our
+                founder completed his Software Engineering bootcamp in April 2023, we have 
+                been on a journey towards building software solutions that respond timely to our clients needs. 
+                </p>
+                
+                <p>We have often been inspired by the need to court simplicity in our approach to problem solving. 
+                A look into our core values may somewhat get one off-track in that it contains terms such as industry 
+                and integrity, especially the industry part. It is worth noting that “industry” as a core value is 
+                among the key factors that has often guided our founder ever since he was in primary school. 
+                Just to provide a little background into the two core values of “industry” and “integrity”, 
+                they were part of the founder’s primary school motto and are the qualities that have guided him
+                 into establishing a company that would help disrupt an industry that has often fallen prey to 
+                 ‘story za jaba’ with regards to meeting client deadlines.</p>
+                 
+                 <p> As we look forward to the future, we remain steadfast in our commitment to excellence. 
+                    V12 Technologies envisions a tomorrow where technology enhances lives, and we stand ready 
+                    to create, innovate, and make a lasting impact through our web and mobile app development 
+                    solutions. Our journey is just beginning, and the best is yet to come. </p>
 
             <h1>Our Mission</h1>
-            <p>V12 Technologies is a tech company that deals in web development, mobile 
-                app development, and other forms of tech support. V12 is counted
-                for its expertise in web solutions and top ranking business portals.
-                V12's invincible expertise and rich experience has raised its spirit
-                to reach ahead from its clients' expectations. Commendable success
-                rate of its other portals is a live paradigm of its work excellence.
+            <p> At V12 Technologies, our mission is to empower businesses and individuals
+                with a dynamic online presence. We are a comprehensive digital solutions provider,
+                specializing in website development encompassing functions such as web hosting, 
+                domain registration, web design and redesign, graphics design, SEO, and web content management.
+                We are also mobile app developers. </p>
+
+                <p> Our commitment is to deliver innovative, user-friendly, and visually captivating websites
+                and mobile apps that represent our clients' unique identities. We provide reliable web 
+                hosting and domain registration services to ensure seamless online accessibility. Our 
+                creative design team crafts captivating visuals and user interfaces, while our content 
+                management experts optimize web content for maximum impact. </p>
+
+               <p> With unwavering dedication to excellence, we seek to help our clients thrive in the digital
+                landscape, providing them with the tools and support they need to succeed.
             </p>
 
             <h1>Our Vision</h1>
-            <p>V12 Technologies is a tech company that deals in web development, mobile 
-                app development, and other forms of tech support. V12 is counted
-                for its expertise in web solutions and top ranking business portals.
-                V12's invincible expertise and rich experience has raised its spirit
-                to reach ahead from its clients' expectations. Commendable success
-                rate of its other portals is a live paradigm of its work excellence.
-            </p>
+            <p>"As a versatile digital solutions company, we envision elevating businesses through cutting-edge web 
+                and mobile experiences that inspire success."</p>
+
+            <h1>Our Core values</h1>
+            <h3>Industry</h3>
+            <p>We place a high value on initiative, dedication, and a strong work ethic. Our team members consistently
+               take the initiative to deliver high-quality solutions and meet client needs promptly. While at it, we 
+               pay meticulous attention to detail, ensuring that all projects are executed with precision and excellence. 
+               We are guided by the need to diligently understand and meet client objectives whilst providing reliable, 
+               effective solutions.</p>
+            <h3>Integrity</h3>
+            <p>We prioritize truthfulness and openness in all interactions with clients, partners, and team members. We 
+            seek to always provide clear and accurate information to ensure trust is guaranteed and maintained. Our 
+            quest for integrity is also guided by the upholding of high ethical standards – a practice which is 
+            non-negotiable. We adhere to a strict code of ethics, ensuring all business decisions and actions are 
+            morally sound.</p>
+            <h3>Professionalism</h3>
+            <p>We adhere to the highest industry standards in all of our operations, ensuring the delivery of
+               top-notch services. We employ and develop a skilled and knowledgeable team of professionals who
+                are experts in their respective fields. To ensure that we deliver relevant solutions to our customers
+                 at all times, we adopt a client-centric approach that helps meet client needs and expectations with 
+                 punctuality, courtesy, and open communication.</p>
         </div>
 
     )
